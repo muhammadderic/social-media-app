cb2d9a09fcdd4ffd9485bc1986ceabc580396afc
muhammadderic <muhammad.donny.e@gmail.com>
2024-08-21 13:00:53 +0700

    Create profile page

---
+++ frontend/src/app/profile/[username]/page.tsx
@@ -0,0 +1,66 @@
+import Feed from "@/components/feed/Feed"
+import LeftMenu from "@/components/leftMenu/LeftMenu"
+import RightMenu from "@/components/rightMenu/RightMenu"
+import Image from "next/image"
+
+const ProfilePage = () => {
+  return (
+    <div className="flex gap-6 pt-6">
+      {/* LEFT MENU */}
+      <div className="hidden xl:block w-[20%]">
+        <LeftMenu />
+      </div>
+
+      {/* MAIN */}
+      <div className="w-full lg:w-[70%] xl:w-[50%]">
+        <div className="flex flex-col gap-6">
+          <div className="flex flex-col items-center justify-center">
+            <div className="w-full h-64 relative">
+              <Image
+                src={"/addImage.png"}
+                alt=""
+                fill
+                className="rounded-md object-cover"
+              />
+              <Image
+                src={"/noAvatar.png"}
+                alt=""
+                width={128}
+                height={128}
+                className="w-32 h-32 rounded-full absolute left-0 right-0 m-auto -bottom-16 ring-4 ring-white object-cover"
+              />
+            </div>
+
+            <h1 className="mt-20 mb-4 text-2xl font-medium">
+              deric deric
+            </h1>
+
+            <div className="flex items-center justify-center gap-12 mb-4">
+              <div className="flex flex-col items-center">
+                <span className="font-medium">0</span>
+                <span className="text-sm">Posts</span>
+              </div>
+              <div className="flex flex-col items-center">
+                <span className="font-medium">0</span>
+                <span className="text-sm">Followers</span>
+              </div>
+              <div className="flex flex-col items-center">
+                <span className="font-medium">0</span>
+                <span className="text-sm">Following</span>
+              </div>
+            </div>
+          </div>
+
+          <Feed />
+        </div>
+      </div>
+
+      {/* RIGHT MENU */}
+      <div className="hidden lg:block w-[30%]">
+        <RightMenu />
+      </div>
+    </div>
+  )
+}
+
+export default ProfilePage
