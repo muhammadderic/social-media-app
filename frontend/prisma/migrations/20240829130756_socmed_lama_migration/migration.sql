fceb77e797d68b59b62aaf609793034ec70cba29
muhammadderic <muhammad.donny.e@gmail.com>
2024-08-29 20:09:00 +0700

    Initialize prisma schema and make initial migration

---
+++ frontend/prisma/migrations/20240829130756_socmed_lama_migration/migration.sql
@@ -0,0 +1,29 @@
+-- CreateTable
+CREATE TABLE `User` (
+    `id` VARCHAR(191) NOT NULL,
+    `username` VARCHAR(191) NOT NULL,
+    `avatar` VARCHAR(191) NULL,
+    `cover` VARCHAR(191) NULL,
+    `name` VARCHAR(191) NULL,
+    `surname` VARCHAR(191) NULL,
+    `desc` VARCHAR(191) NULL,
+    `city` VARCHAR(191) NULL,
+    `school` VARCHAR(191) NULL,
+    `work` VARCHAR(191) NULL,
+    `website` VARCHAR(191) NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    UNIQUE INDEX `User_username_key`(`username`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Post` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `desc` VARCHAR(191) NOT NULL,
+    `img` VARCHAR(191) NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
